43c18115ea211b7f6655d99b2dfd4e9979c97e75
Sardorbek031 <w72075433@gmail.com>
2025-10-26 15:31:06 +0500

    finish the models lesson

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Ma'lumotlar basasi uchun table
+class Person(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    about = models.CharField(max_length=255)
+    admin_or_not = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Ismi: {self.first_name}, familyasi: {self.last_name}"
--- my_project/settings.py
+++ my_project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Or the IP address/hostname of your PostgreSQL server
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
