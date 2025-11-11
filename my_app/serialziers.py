dc5505a6518234fd58f128ffa9cf24fb38be9a38
Sardorbek031 <w72075433@gmail.com>
2025-11-11 18:50:53 +0500

    serializer and view

---
+++ my_app/serialziers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Person
+
+class PersonModelSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    first_name = serializers.CharField(max_length=25)
+    last_name = serializers.CharField(max_length=25)
+    about = serializers.CharField(max_length=255)
+    admin_or_not = serializers.BooleanField(default=False)
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.about = validated_data.get("about", instance.about)
+        instance.admin_or_not = validated_data.get("admin_or_not", instance.admin_or_not)
+        instance.save()
+        return instance
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Person
+from .serialziers import PersonModelSerializer
 
-# Bu predtsavleniya
+class PersonListApiView(APIView):
+    def get(self, request):
+        persons = Person.objects.all()
+        serializer = PersonModelSerializer(persons, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PersonModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
